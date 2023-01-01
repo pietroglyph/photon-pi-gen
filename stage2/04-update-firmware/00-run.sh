02a6fba2b252da3bf09103b95ef02d3393ad783a
Matt <matthew.morley.ca@gmail.com>
2022-12-31 22:08:01 -0800

    Delete more extra packages and docs and stuff
    Remove Pi 4 support, as well as 32-bit support

--- stage2/04-update-firmware/00-run.sh
+++ stage2/04-update-firmware/00-run.sh
@@ -3,7 +3,16 @@
 echo "Updating firmware to cb852c1fa666"
 
 on_chroot << EOF
-SKIP_WARNING=1 rpi-update cb852c1fa666
+SKIP_WARNING=1 PRUNE_MODULES=1 rpi-update cb852c1fa666
 
 rm -rf /var/lib/apt/lists/*
+apt-get clean
+rm -rf /usr/share/locale/
+rm -rf /usr/share/doc
+apt purge 'x11-*'
+rm /boot/start_db.elf /boot/start4db.elf /boot/start4x.elf /boot/start4cd.elf /boot/start_cd.elf
+rm /boot/kernel7.img /boot/kernel7l.img /boot/kernel.img
+rm -rf /opt/vc
+rm -rf /boot.bak
+
 EOF
