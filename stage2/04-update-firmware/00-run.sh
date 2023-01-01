cd1d3e8f175ff31869164f5bfec584db571f87da
Matt <matthew.morley.ca@gmail.com>
2023-01-01 08:58:46 -0800

    Move apt list clean to after JRE install

--- stage2/04-update-firmware/00-run.sh
+++ stage2/04-update-firmware/00-run.sh
@@ -5,10 +5,6 @@ echo "Updating firmware to cb852c1fa666"
 on_chroot << EOF
 SKIP_WARNING=1 PRUNE_MODULES=1 rpi-update cb852c1fa666
 
-rm -rf /var/lib/apt/lists/*
-apt-get clean
-rm -rf /usr/share/locale/
-rm -rf /usr/share/doc
 apt purge -y 'x11-*'
 rm /boot/start_db.elf /boot/start4db.elf /boot/start4x.elf /boot/start4cd.elf /boot/start_cd.elf
 rm /boot/kernel7.img /boot/kernel7l.img /boot/kernel.img
@@ -18,5 +14,10 @@ rm -rf /boot.bak
 apt-get purge -y python3 gdb gcc g++ default-jdk
 apt-get autoremove -y
 apt-get install -y openjdk-11-jre
+
+rm -rf /var/lib/apt/lists/*
+apt-get clean
+rm -rf /usr/share/doc
+rm -rf /usr/share/locale/
 
 EOF
