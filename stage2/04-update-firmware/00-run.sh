db12b7fb0721becd323c3acc16b6b8c8f0adb66c
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2023-01-12 17:10:42 -0800

    Don't update firmware at all (for testing)

--- stage2/04-update-firmware/00-run.sh
+++ stage2/04-update-firmware/00-run.sh
@@ -3,8 +3,6 @@
 echo "Updating firmware to cb852c1fa666"
 
 on_chroot << EOF
-SKIP_WARNING=1 PRUNE_MODULES=1 rpi-update cb852c1fa666
-
 apt purge -y 'x11-*'
 rm /boot/start_db.elf /boot/start4db.elf /boot/start4x.elf /boot/start4cd.elf /boot/start_cd.elf
 rm /boot/kernel7.img /boot/kernel7l.img /boot/kernel.img
