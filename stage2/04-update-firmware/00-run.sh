a60f02b0e8d37ce50423b208703d647539190e0d
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2023-01-12 17:13:04 -0800

    Update firmware but don't prune kernel modules (for testing)

--- stage2/04-update-firmware/00-run.sh
+++ stage2/04-update-firmware/00-run.sh
@@ -3,7 +3,7 @@
 echo "Updating firmware to cb852c1fa666"
 
 on_chroot << EOF
-SKIP_WARNING=1 PRUNE_MODULES=1 rpi-update cb852c1fa666
+SKIP_WARNING=1 rpi-update cb852c1fa666
 
 apt purge -y 'x11-*'
 rm /boot/start_db.elf /boot/start4db.elf /boot/start4x.elf /boot/start4cd.elf /boot/start_cd.elf
